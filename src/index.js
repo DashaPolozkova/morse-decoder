353224eca833c83fb7e0652c9b18d5b41b03b1b6
Darya Polozkova <dasha.priemko@gmail.com>
2022-02-17 23:59:11 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -39,7 +39,24 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
-}
+     let arr = [];
+     let newArr= []
+    for (let i = 0; i < expr.length; i+=10) {
+       arr.push(expr.slice(i, i + 10))
+    }
+    for (let j = 0; j < arr.length; j++) {
+      newArr.push(arr[j].replace(/10/g, '.').replace(/11/g, '-').replace(/0/g, ''))
+    }
+    for (let k = 0; k < newArr.length; k++) {
+      if (newArr[k] === '**********') { 
+        newArr[k] = ' '
+      } else {
+        newArr[k] = MORSE_TABLE[newArr[k]]
+      }
+    }
+    let result = newArr.join('')
+  return result }
+ 
 
 module.exports = {
     decode
